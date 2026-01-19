4ed68a69ae880bdb4a6cb272948b82f4205ab5cb
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-19 11:35:18 +0530

    Refactor blog page to use partials for hero and blog area
    Extracted the blog hero and blog area sections into separate Blade partials (hero.blade.php and blogarea.blade.php) and updated index.blade.php to include these partials. This improves code organization and reusability for the blog frontend views.

---
+++ resources/views/frontend/blog/blogarea.blade.php
@@ -0,0 +1,194 @@
+<section class="th-blog-wrapper space-top space-extra-bottom">
+        <div class="container">
+            <div class="row gy-40">
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-1.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">How to Keep Your Loved Ones Healthy Year-Round </a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-2.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">How to Look After Dogs Loved Ones Healthy Year-Round</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-3.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">Signs Your Cat is Stressed Your Loved Ones </a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-4.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">Essential Tips for a Healthy Pet Life Throughout the Year</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-5.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">Understanding Dog Behavior for Better Communication</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-6.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">The Ultimate Guide to Fostering Rescue Animals</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-7.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">The Importance of Regular Vet Check-Ups for Pets</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-8.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">How to Create a Pet-Friendly Home Environment</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-md-6" data-cue="slideInUp">
+                    <div class="blog-card">
+                        <div class="blog-img">
+                            <a href="{{ route('blogdetails') }}">
+                                <img src="assets/img/blog/blog-s-1-3.jpg" alt="blog image">
+                            </a>
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="{{ route('blog') }}"><i class="fas fa-user"></i>Jordan Park</a>
+                                <a href="{{ route('blog') }}"><i class="fas fa-calendar"></i>24 Jun, 2025</a>
+                            </div>
+                            <h3 class="box-title"><a href="{{ route('blogdetails') }}">A Comprehensive Handbook for Nurturing Rescue Pets</a></h3>
+                            <div class="btn-wrap">
+                                <a href="{{ route('blogdetails') }}" class="link-btn th-btn-icon">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="th-pagination mt-50 text-center" data-cue="slideInUp">
+                <ul>
+                    <li><a href="#"><i class="far fa-arrow-left"></i></a></li>
+                    <li><a class="active" href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#"><i class="far fa-arrow-right"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
