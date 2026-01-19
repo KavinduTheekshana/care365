0ab1f7f3f3e5a4e61454c60ced9cf439fe720603
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-19 15:36:00 +0530

    Update marquee slider content and icons
    Replaced marquee slider items with new text values and updated all icons to use 'marquee-icon3-1.png' instead of the previous SVG. Also added a hidden icon for the 'Expertise' slide and adjusted some text for consistency.

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -34,84 +34,79 @@ Video Area
     <!--==============================
 Marquee Area  
 ==============================-->
-        <div class="overflow-hidden space-bottom">
-            <div class="container-fluid p-0" data-cue="slideInUp">
-                <div class="swiper th-slider marquee-slider1" data-slider-options='{"breakpoints":{"0":{"slidesPerView":"auto"}},"autoplay":{"delay":0,"disableOnInteraction":false},"noSwiping":"true","speed":10000,"spaceBetween":0}'>
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Compassionate Care
-                                </a>
+    <div class="overflow-hidden space-bottom">
+        <div class="container-fluid p-0" data-cue="slideInUp">
+            <div class="swiper th-slider marquee-slider1" data-slider-options='{"breakpoints":{"0":{"slidesPerView":"auto"}},"autoplay":{"delay":0,"disableOnInteraction":false},"noSwiping":"true","speed":10000,"spaceBetween":0}'>
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                Comfort </a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Dignity & Respect
-                                </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                Quality of Life </a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Personalized Support
-                                </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                Continuum of Care </a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Friendly Staff
-                                </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                friendly Staff </a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Customized Care Plans
-                                </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                Customized Care Plans </a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Holistic Wellness
-                                </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                Holistic Approach </a>
                         </div>
-                        <div class="swiper-slide">
-                            <div class="marquee-card">
-                                <div class="marquee-icon">
-                                    <img src="assets/img/icon/marquee-icon1-1.svg" alt="img">
-                                </div>
-                                <a target="_blank" href="#">
-                                    Peace of Mind
-                                </a>
+                    </div>
+                    
+                    <div class="swiper-slide">
+                        <div class="marquee-card">
+                            <div class="marquee-icon hidden">
+                                <img src="assets/img/icon/marquee-icon3-1.png" alt="img">
                             </div>
+                            <a target="_blank" href="#">
+                                Expertise </a>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
+    </div>
     <!--==============================
 Process Area  
 ==============================-->
