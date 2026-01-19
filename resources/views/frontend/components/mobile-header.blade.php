dc417495361468bd0167582bbf9927b3f45e5cdf
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-19 08:25:15 +0530

    Update mobile header to use route helpers
    Replaced hardcoded links for Home and About Us with Laravel route helpers in the mobile header component for improved maintainability and consistency.

--- resources/views/frontend/components/mobile-header.blade.php
+++ resources/views/frontend/components/mobile-header.blade.php
@@ -2,12 +2,12 @@
         <div class="th-menu-area text-center">
             <button class="th-menu-toggle"><i class="fal fa-times"></i></button>
             <div class="mobile-logo">
-                <a href="index.html"><img src="assets/img/logo3.png" alt="Care 365"></a>
+                <a href="{{ route('home') }}"><img src="assets/img/logo3.png" alt="Care 365"></a>
             </div>
             <div class="th-mobile-menu">
                 <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('about') }}">About Us</a></li>
                     <li><a href="service.html">Services</a></li>
                     <li><a href="gallery.html">Gallery</a></li>
                     <li><a href="testimonial.html">Testimonial</a></li>
