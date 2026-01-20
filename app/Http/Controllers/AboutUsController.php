b85d92e3577c38e00301754d0011e23348ddd7b6
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-20 15:34:04 +0530

    Update service section on the about us page

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Service;
 
 class AboutUsController extends Controller
 {
     public function index(): View
     {
-        return view('frontend.about.index');
+        // Get random 8 services for the slider
+        $services = Service::where('is_public', true)
+                          ->inRandomOrder()
+                          ->take(8)
+                          ->get();
+        
+        return view('frontend.about.index', compact('services'));
     }
 }
