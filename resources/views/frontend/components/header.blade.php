8b20ea19386c48e8be87daab1232f386cd29786b
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-20 18:58:18 +0530

    Update header.blade.php

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -1,51 +1,75 @@
- <header class="th-header header-default">
-        <div class="sticky-wrapperr">
-            <!-- Main Menu Area -->
-            <div class="menu-area">
-                <div class="container">
-                    <div class="row align-items-center justify-content-between">
-                        <div class="col-auto">
-                            <div class="header-logo">
-                                <a href="{{ route('home') }}">
-                                    <img src="assets/img/logo.png" alt="Care 365">
-                                </a>
-                            </div>
+<header class="th-header header-default">
+    <div class="sticky-wrapperr">
+        <!-- Main Menu Area -->
+        <div class="menu-area">
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-auto">
+                        <div class="header-logo">
+                            <a href="{{ route('home') }}">
+                                <img src="assets/img/logo.png" alt="Care 365">
+                            </a>
                         </div>
-                        <div class="col-auto me-xxl-auto">
-                            <nav class="main-menu d-none d-lg-inline-block">
-                                <ul>
-                                    <li><a href="{{ route('home') }}">Home</a></li>
-                                    <li><a href="{{ route('about') }}">About Us</a></li>
-                                    <li><a href="{{route('services')}}">Services</a></li>
-                                    <li><a href="{{route('gallery')}}">Gallery</a></li>
-                                    <li><a href="{{route('testimonial')}}">Testimonial</a></li>
-                                    <li><a href="{{route('faq')}}">FAQ'S</a></li>
-                                    <li><a href="{{ route('blog') }}">Blog</a></li>
-                                    <li><a href="{{ route('contact') }}">Contact Us</a></li>
-                                </ul>
-                            </nav>
-                            <div class="header-button d-flex d-lg-none">
-                                <button type="button" class="th-menu-toggle icon-btn"><i
-                                        class="far fa-bars"></i></button>
-                            </div>
+                    </div>
+                    <div class="col-auto me-xxl-auto">
+                        <nav class="main-menu d-none d-lg-inline-block">
+                            <ul>
+                                <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                                    <a href="{{ route('home') }}">Home</a>
+                                </li>
+                                <li class="{{ request()->routeIs('about') ? 'active' : '' }}">
+                                    <a href="{{ route('about') }}">About Us</a>
+                                </li>
+                                <li class="{{ request()->routeIs('services') ? 'active' : '' }}">
+                                    <a href="{{route('services')}}">Services</a>
+                                </li>
+                                <li class="{{ request()->routeIs('gallery') ? 'active' : '' }}">
+                                    <a href="{{route('gallery')}}">Gallery</a>
+                                </li>
+                                <li class="{{ request()->routeIs('testimonial') ? 'active' : '' }}">
+                                    <a href="{{route('testimonial')}}">Testimonial</a>
+                                </li>
+                                <li class="{{ request()->routeIs('faq') ? 'active' : '' }}">
+                                    <a href="{{route('faq')}}">FAQ'S</a>
+                                </li>
+                                <li class="{{ request()->routeIs('blog') ? 'active' : '' }}">
+                                    <a href="{{ route('blog') }}">Blog</a>
+                                </li>
+                                <li class="{{ request()->routeIs('contact') ? 'active' : '' }}">
+                                    <a href="{{ route('contact') }}">Contact Us</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div class="header-button d-flex d-lg-none">
+                            <button type="button" class="th-menu-toggle icon-btn"><i
+                                    class="far fa-bars"></i></button>
                         </div>
-                        <div class="col-auto d-xl-block d-none">
-                            <div class="header-button">
-                                <div class="info-card">
-                                    <div class="box-icon">
-                                        <img src="assets/img/icon/phone-white.svg" alt="phon-icon">
-                                    </div>
-                                    <div class="box-content">
-                                        <h4 class="box-title text-white">Call us: </h4>
-                                        <p class="box-text"><a class="text-white" href="tel:94776604040">(+94)77 660
-                                                40 40</a></p>
-                                    </div>
+                    </div>
+                    <div class="col-auto d-xl-block d-none">
+                        <div class="header-button">
+                            <div class="info-card">
+                                <div class="box-icon">
+                                    <img src="assets/img/icon/phone-white.svg" alt="phon-icon">
                                 </div>
-                                {{-- <a href="{{ route('contact') }}" class="th-btn style-border">Book Now</a> --}}
+                                <div class="box-content">
+                                    <h4 class="box-title text-white">Call us: </h4>
+                                    <p class="box-text"><a class="text-white" href="tel:94776604040">(+94)77 660
+                                            40 40</a></p>
+                                </div>
                             </div>
+                            {{-- <a href="{{ route('contact') }}" class="th-btn style-border">Book Now</a> --}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </header>
+    </div>
+</header>
+
+
+<style>
+.main-menu ul li.active a {
+    color: #3A8DCA !important; 
+}
+
+</style>
