7c9f92ab5d878bf884e3c2be22348e2d5f6c4e41
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-20 15:49:39 +0530

    Update testimonial section on about us page

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -7,6 +7,8 @@ use Illuminate\View\View;
 use App\Models\Service;
 use App\Models\Whoweare;
 use App\Models\CareHome;
+use App\Models\Testimonial;
+
 
 class AboutUsController extends Controller
 {
@@ -27,7 +29,12 @@ class AboutUsController extends Controller
         $carehomes = CareHome::where('is_public', true)
                             ->orderBy('created_at', 'desc')
                             ->get();
+
+        $testimonials = Testimonial::where('is_public', true)
+                            ->inRandomOrder()
+                             ->take(4)
+                            ->get();
         
-        return view('frontend.about.index', compact('services', 'whoweares', 'carehomes'));
+        return view('frontend.about.index', compact('services', 'whoweares', 'carehomes', 'testimonials'));
     }
 }
--- resources/views/frontend/about/testimonial.blade.php
+++ resources/views/frontend/about/testimonial.blade.php
@@ -1,126 +1,67 @@
-    <section class="testi-area-1 space overflow-hidden bg-white" id="testi-sec">
-        <!-- <div class="service-bg-shape1-1 shape-mockup shake d-xxl-block d-none" data-top="20%" data-left="5%">
-            <img src="assets/img/shape/shape1-18.png" alt="img">
+<section class="testi-area-1 space overflow-hidden bg-white" id="testi-sec">
+    <div class="container">
+        <div class="title-area text-center">
+            <span class="sub-title text-anim subtitle-line" data-cue="slideInUp">Testimonials</span>
+            <h2 class="sec-title text-anim" data-cue="slideInUp">What Families Say About Us</h2>
+            <p class="fs-18 text-anim2" data-cue="slideInUp">Read the heartwarming stories from families who entrusted their loved ones to our care.</p>
         </div>
-        <div class="service-bg-shape1-2 shape-mockup shake d-xxl-block d-none" data-top="20%" data-right="5%">
-            <img src="assets/img/shape/shape1-19.png" alt="img">
-        </div>
-        <div class="service-bg-shape1-3 shape-mockup heartbeat d-xxl-block d-none" data-bottom="27%" data-left="5%">
-            <img src="assets/img/shape/shape1-20.png" alt="img">
-        </div>
-        <div class="service-bg-shape1-3 shape-mockup jump d-xxl-block d-none" data-top="65%" data-right="5%">
-            <img src="assets/img/shape/shape1-21.png" alt="img">
-        </div>
-        <div class="service-bg-shape1-3 shape-mockup moving d-xxl-block d-none" data-bottom="0%" data-left="5%">
-            <img src="assets/img/shape/shape1-22.png" alt="img">
-        </div> -->
-        <div class="container">
-            <div class="title-area text-center">
-                <span class="sub-title text-anim subtitle-line" data-cue="slideInUp">Testimonials</span>
-                <h2 class="sec-title text-anim" data-cue="slideInUp">What Families Say About Us</h2>
-                <p class="fs-18 text-anim2" data-cue="slideInUp">Read the heartwarming stories from families who entrusted their loved ones to our care.</p>
-            </div>
-            <div class="row gy-50 flex-row-reverse">
-                <div class="slider-area testi-slider1" data-cue="slideInUp">
-                    <div class="swiper th-slider has-shadow" id="testiSlide1" data-slider-options='{"autoHeight": "true","breakpoints":{"0":{"slidesPerView":1},"768":{"slidesPerView":"1"},"992":{"slidesPerView":"1"},"1200":{"slidesPerView":"2"},"1600":{"slidesPerView":"2"}}}'>
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <div class="testi-card">
-                                    <div class="testi-1-quote">
-                                        <img src="assets/img/icon/quote_icon.svg" alt="icon">
-                                    </div>
-                                    <div class="testi-bg-mask" data-mask-src="assets/img/shape/testi_card_mask1_1.jpg"></div>
-                                    <div class="testi-card-profile">
-                                        <div class="testi-card-avater">
-                                            <img src="assets/img/testimonial/testi1-1.jpg" alt="img" width="80" height="80">
-                                        </div>
-                                        <div class="testi-card-profile-detaile">
-                                            <h3 class="box-title">Ruwan de Silva</h3>
-                                            <p class="box-desig">Senior Software Engineer</p>
-                                        </div>
-                                    </div>
-
-                                    <div class="testi-card_review">
-                                        <i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i>
-                                    </div>
-                                    <p class="box-text">“Although expert groomers themselves, lots of cats like being brushed and it’s a great way of bonding with your cat. As well as fewer hairballs and less shedding, grooming your cats will imitate how they groom each other naturally and for you as a result.”</p>
+        
+        @if($testimonials->count() > 0)
+        <div class="row gy-50 flex-row-reverse">
+            <div class="slider-area testi-slider1" data-cue="slideInUp">
+                <div class="swiper th-slider has-shadow" id="testiSlide1" data-slider-options='{"autoHeight": false,"breakpoints":{"0":{"slidesPerView":1},"768":{"slidesPerView":"1"},"992":{"slidesPerView":"1"},"1200":{"slidesPerView":"2"},"1600":{"slidesPerView":"2"}}}'>
+                    <div class="swiper-wrapper">
+                        @foreach($testimonials as $testimonial)
+                        <div class="swiper-slide">
+                            <div class="testi-card">
+                                <div class="testi-1-quote">
+                                    <img src="assets/img/icon/quote_icon.svg" alt="icon">
                                 </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="testi-card">
-                                    <div class="testi-1-quote">
-                                        <img src="assets/img/icon/quote_icon.svg" alt="icon">
+                                <div class="testi-bg-mask" data-mask-src="assets/img/shape/testi_card_mask1_1.jpg"></div>
+                                <div class="testi-card-profile">
+                                    <div class="testi-card-avater">
+                                        @if($testimonial->image_path)
+                                        <img src="{{ asset('testimonial_img/' . $testimonial->image_path) }}" alt="{{ $testimonial->name }}" width="80" height="80">
+                                        @else
+                                        <img src="assets/img/testimonial/dummy.jpg" alt="{{ $testimonial->name }}" width="80" height="80">
+                                        @endif
                                     </div>
-                                    <div class="testi-bg-mask" data-mask-src="assets/img/shape/testi_card_mask1_1.jpg"></div>
-                                    <div class="testi-card-profile">
-                                        <div class="testi-card-avater">
-                                            <img src="assets/img/testimonial/testi1-2.jpg" alt="img" width="80" height="80">
-                                        </div>
-                                        <div class="testi-card-profile-detaile">
-                                            <h3 class="box-title">Silvia Peiris</h3>
-                                            <p class="box-desig">Bank Officer</p>
-                                        </div>
+                                    <div class="testi-card-profile-detaile">
+                                        <h3 class="box-title">{{ $testimonial->name }}</h3>
+                                        <p class="box-desig">{{ $testimonial->position }}</p>
                                     </div>
-
-                                    <div class="testi-card_review">
-                                        <i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i>
-                                    </div>
-                                    <p class="box-text">“Regular brushing doesn’t just keep your cat’s coat clean it also mimics natural grooming behavior and builds a deeper sense of trust you and your furry friend. Even though cats groom themselves daily, they often enjoy the sensation of a brush. It’s not just good for.”</p>
                                 </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="testi-card">
-                                    <div class="testi-1-quote">
-                                        <img src="assets/img/icon/quote_icon.svg" alt="icon">
-                                    </div>
-                                    <div class="testi-bg-mask" data-mask-src="assets/img/shape/testi_card_mask1_1.jpg"></div>
-                                    <div class="testi-card-profile">
-                                        <div class="testi-card-avater">
-                                            <img src="assets/img/testimonial/testi1-3.jpg" alt="img" width="80" height="80">
-                                        </div>
-                                        <div class="testi-card-profile-detaile">
-                                            <h3 class="box-title">Jeewan Mahanama</h3>
-                                            <p class="box-desig">Senior Software Engineer</p>
-                                        </div>
-                                    </div>
 
-                                    <div class="testi-card_review">
-                                        <i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i>
-                                    </div>
-                                    <p class="box-text">“Brushing your cat helps reduce shedding and while giving them the same comfort and feel when being by another cat. Cats are natural self-cleaners, but they still love the extra attention. time can become a peaceful moment of between owner and pet.”</p>
+                                <div class="testi-card_review">
+                                    @for($i = 1; $i <= 5; $i++)
+                                        @if($i <= $testimonial->rating)
+                                        <i class="fa-sharp fa-solid fa-star"></i>
+                                        @else
+                                        <i class="fa-sharp fa-regular fa-star"></i>
+                                        @endif
+                                    @endfor
+                                    <span class="rating-number ms-2">({{ $testimonial->rating }}/5)</span>
                                 </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="testi-card">
-                                    <div class="testi-1-quote">
-                                        <img src="assets/img/icon/quote_icon.svg" alt="icon">
-                                    </div>
-                                    <div class="testi-bg-mask" data-mask-src="assets/img/shape/testi_card_mask1_1.jpg"></div>
-                                    <div class="testi-card-profile">
-                                        <div class="testi-card-avater">
-                                            <img src="assets/img/testimonial/testi1-4.jpg" alt="img" width="80" height="80">
-                                        </div>
-                                        <div class="testi-card-profile-detaile">
-                                            <h3 class="box-title">Arjuna Perera</h3>
-                                            <p class="box-desig">Bank Officer</p>
-                                        </div>
-                                    </div>
-
-                                    <div class="testi-card_review">
-                                        <i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i><i class="fa-sharp fa-solid fa-star"></i>
-                                    </div>
-                                    <p class="box-text">“Helping your cat stay clean through regular brushing keeps their coat healthy, and it also mimics the social receive in the wild — making you part of their “family. While your cat may be skilled at self-grooming, your touch adds boosting the between you both.”</p>
-                                </div>
+                                
+                                <p class="box-text">"{{ $testimonial->message }}"</p>
                             </div>
                         </div>
-                        <div class="slider-pagination-wrap mt-30">
-                            <button data-slider-prev="#testiSlide1" class="slider-arrow default"><i class="far fa-arrow-left"></i></button>
-                            <div class="slider-pagination"></div>
-                            <button data-slider-next="#testiSlide1" class="slider-arrow default"><i class="far fa-arrow-right"></i></button>
-                        </div>
+                        @endforeach
+                    </div>
+                    <div class="slider-pagination-wrap mt-30">
+                        <button data-slider-prev="#testiSlide1" class="slider-arrow default"><i class="far fa-arrow-left"></i></button>
+                        <div class="slider-pagination"></div>
+                        <button data-slider-next="#testiSlide1" class="slider-arrow default"><i class="far fa-arrow-right"></i></button>
                     </div>
                 </div>
-
             </div>
         </div>
-    </section>
+        @else
+        <div class="text-center py-5">
+            <div class="alert alert-info">
+                <p>No testimonials available at the moment.</p>
+            </div>
+        </div>
+        @endif
+    </div>
+</section>
