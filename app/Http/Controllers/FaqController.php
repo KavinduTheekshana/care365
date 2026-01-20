90a83218763aef59599339ff510650d05d76fd33
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-20 16:17:44 +0530

    Update Faq Page

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
+use App\Models\Faq;
 
 class FaqController extends Controller
 {
-
     public function index(): View
     {
-        return view('frontend.faq.index');
+        // Get all FAQs where visibility is true
+        $faqs = Faq::where('visibility', true)
+                   ->orderBy('created_at', 'desc')
+                   ->get();
+        
+        return view('frontend.faq.index', compact('faqs'));
     }
-
 }
