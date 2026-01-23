569477ce5e59556fcd3693245eb2aac9c494c5cc
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-23 09:29:22 +0530

    Create successful stories section

--- resources/views/frontend/home/stories.blade.php
+++ resources/views/frontend/home/stories.blade.php
@@ -1,58 +1,50 @@
 <div class="overflow-hidden space z-index-common">
+    <div class="container-fluid p-0">
+        <div class="title-area text-center">
+            <span class="sub-title style2 text-anim" data-cue="slideInUp">Recent stories</span>
+            <h2 class="sec-title text-anim" data-cue="slideInUp">Our success stories</h2>
+            <p class="fs-18 text-anim2" data-cue="slideInUp">Easily find and book trusted Pet Caregivers near you</p>
+        </div>
 
+        <div class="gallery-1-wrap">
+            @php
+                $storyIndex = 0;
+                $totalStories = count($successStories);
+            @endphp
 
-        <div class="container-fluid p-0">
-            <div class="title-area text-center">
-                <span class="sub-title style2 text-anim" data-cue="slideInUp">Recent stories</span>
-                <h2 class="sec-title text-anim" data-cue="slideInUp">Our success stories</h2>
-                <p class="fs-18 text-anim2" data-cue="slideInUp">Easily find and book trusted Pet Caregivers near you</p>
-            </div>
-
-            <div class="gallery-1-wrap">
-
-                <div class="gallery-card">
-                    <div class="gallery-img">
-                        <img src="assets/img/Home-img/galary-img5.png" alt="img">
-                        <a href="assets/img/Home-img/galary-img5.png" class="icon-btn th-popup-image"><i class="fa-regular fa-magnifying-glass"></i></a>
-                    </div>
-                </div>
-
-                <div class="gallery-card-wrap">
-                    <div class="gallery-card">
-                        <div class="gallery-img">
-                            <img src="assets/img/Home-img/galaryimg1.png" alt="img">
-                            <a href="assets/img/Home-img/galaryimg1.png" class="icon-btn th-popup-image"><i class="fa-regular fa-magnifying-glass"></i></a>
+            @while($storyIndex < $totalStories)
+                @if($storyIndex % 3 === 0)
+                    {{-- Single large image (left side) --}}
+                    @if(isset($successStories[$storyIndex]))
+                        <div class="gallery-card">
+                            <div class="gallery-img">
+                                <img src="{{ asset('success_stories_img/' . $successStories[$storyIndex]->image) }}" alt="{{ $successStories[$storyIndex]->image_alt ?? $successStories[$storyIndex]->title }}">
+                                <a href="{{ asset('success_stories_img/' . $successStories[$storyIndex]->image) }}" class="icon-btn th-popup-image">
+                                    <i class="fa-regular fa-magnifying-glass"></i>
+                                </a>
+                            </div>
                         </div>
+                        @php $storyIndex++; @endphp
+                    @endif
+                @else
+                    {{-- Paired images (right side) --}}
+                    <div class="gallery-card-wrap">
+                        @for($i = 0; $i < 2; $i++)
+                            @if(isset($successStories[$storyIndex]))
+                                <div class="gallery-card">
+                                    <div class="gallery-img">
+                                        <img src="{{ asset('success_stories_img/' . $successStories[$storyIndex]->image) }}" alt="{{ $successStories[$storyIndex]->image_alt ?? $successStories[$storyIndex]->title }}">
+                                        <a href="{{ asset('success_stories_img/' . $successStories[$storyIndex]->image) }}" class="icon-btn th-popup-image">
+                                            <i class="fa-regular fa-magnifying-glass"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                @php $storyIndex++; @endphp
+                            @endif
+                        @endfor
                     </div>
-                    <div class="gallery-card">
-                        <div class="gallery-img">
-                            <img src="assets/img/Home-img/galary-img2.png" alt="img">
-                            <a href="assets/img/Home-img/galary-img2.png" class="icon-btn th-popup-image"><i class="fa-regular fa-magnifying-glass"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="gallery-card">
-                    <div class="gallery-img">
-                        <img src="assets/img/Home-img/galary-img6.png" alt="img">
-                        <a href="assets/img/Home-img/galary-img6.png" class="icon-btn th-popup-image"><i class="fa-regular fa-magnifying-glass"></i></a>
-                    </div>
-                </div>
-
-                <div class="gallery-card-wrap">
-                    <div class="gallery-card">
-                        <div class="gallery-img">
-                            <img src="assets/img/Home-img/galary-img3.png" alt="img">
-                            <a href="assets/img/Home-img/galary-img3.png" class="icon-btn th-popup-image"><i class="fa-regular fa-magnifying-glass"></i></a>
-                        </div>
-                    </div>
-                    <div class="gallery-card">
-                        <div class="gallery-img">
-                            <img src="assets/img/Home-img/galary-img4.png" alt="img">
-                            <a href="assets/img/Home-img/galary-img4.png" class="icon-btn th-popup-image"><i class="fa-regular fa-magnifying-glass"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                @endif
+            @endwhile
         </div>
     </div>
+</div>
