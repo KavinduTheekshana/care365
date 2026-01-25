bf13b4c299e544153c01605abf3705e063048581
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-25 20:21:08 +0530

    update about us page (index.blade.php)

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -20,10 +20,14 @@
     @include('frontend.about.hero')
     @include('frontend.about.about')
     @include('frontend.about.journey')
+    <!--
     @include('frontend.about.services')
+    -->
     @include('frontend.about.care')
     @include('frontend.about.homes')
+    <!--
     @include('frontend.about.testimonial')
+    -->
 @endsection
 
 
