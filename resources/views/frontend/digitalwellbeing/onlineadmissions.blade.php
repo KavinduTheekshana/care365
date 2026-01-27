d3ed1476008fd84e4ff971c00c28eb88f05d41d0
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-27 16:03:15 +0530

    Update online admission section

--- resources/views/frontend/digitalwellbeing/onlineadmissions.blade.php
+++ resources/views/frontend/digitalwellbeing/onlineadmissions.blade.php
@@ -8,7 +8,7 @@
                 style="font-size: clamp(1.8rem, 5vw, 2.4rem); line-height: 1.2; margin-bottom: 1rem;">
                 Our Goal Is To Make Your Life Better
             </h2>
-            <p class="fs-18 text-anim2" data-cue="slideInUp">We simplify admissions with our easy online system, allowing you to initiate the process from anywhere globally. Secure online payments are accepted via credit card in multiple currencies, and installment plans are available. Log in to view payments and history.</p>
+            <p class="fs-18" data-cue="slideInUp">We simplify admissions with our easy online system, allowing you to initiate the process from anywhere globally. Secure online payments are accepted via credit card in multiple currencies, and installment plans are available. Log in to view payments and history.</p>
         </div>
         
         <!-- Button wrapper with flexbox for centering -->
