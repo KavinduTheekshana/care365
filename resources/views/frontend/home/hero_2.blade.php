9a10021c74e6de75eabcb2b5ec411103b209cd31
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-01-27 08:37:26 +0530

    update hero section

--- resources/views/frontend/home/hero_2.blade.php
+++ resources/views/frontend/home/hero_2.blade.php
@@ -1,72 +1,32 @@
-<div class="th-hero-wrapper hero-7 " id="hero" style="height: 95vh; ">
+<div class="th-hero-wrapper hero-7" id="hero" style="height: 95vh;">
 
-        <div class="hero-left-social">
-            <ul>
-                <li><span class="hero-border"></span></li>
-                <li class="follow-text">
-                    <h6>FOLLOW US -</h6>
-                </li>
-                <li><a href="https://www.facebook.com/Care36t5"><i class="fab fa-facebook-f"></i></a></li>
-                <li><a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
-                <li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
-            </ul>
-        </div>
+    <div class="hero-left-social">
+        <ul>
+            <li><span class="hero-border"></span></li>
+            <li class="follow-text">
+                <h6>FOLLOW US -</h6>
+            </li>
+            <li><a href="https://www.facebook.com/Care36t5"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
+        </ul>
+    </div>
 
-
-        <div class="swiper th-slider" id="heroSlider8" data-slider-options='{"effect":"fade", "autoplay":true, "autoHeight": "true"}'>
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/Home-img/personalized-care-tailoring-services-to-individual-needs-hero-1.jpg"></div>
-                    <div class="hero-overlay"></div>
-                    <div class="hero-inner">
-                        <div class="container th-container2">
-                            <div class="row gy-4 gx-40 align-items-center justify-content-center">
-                                <div class="col-xxl-8 col-xl-9 col-lg-10">
-                                    <div class="hero-style7">
-                                        <h1 class="hero-title" data-ani="slideinup" data-ani-delay="0.4s">A Home Away From Home for Your Loved Ones</h1>
-                                        <p class="hero-text" data-ani="slideinup" data-ani-delay="0.6s">Safe, comfortable assisted living with personalized senior care</p>
-                                        <div data-ani="slideinup" data-ani-delay="0.10">
-                                            <a href="{{ route('contact') }}" class="th-btn th-btn-icon2">Get Started <i class="far fa-long-arrow-right ms-3"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/Home-img/615b679fef36ca74d291148e_Depositphotos_25178721_L.jpg"></div>
-                    <div class="hero-overlay"></div>
-                    <div class="hero-inner">
-                        <div class="container th-container2">
-                            <div class="row gy-4 gx-40 align-items-center justify-content-center">
-                                <div class="col-xxl-8 col-xl-9 col-lg-10">
-                                    <div class="hero-style7">
-                                        <h1 class="hero-title" data-ani="slideinup" data-ani-delay="0.4s">Where Golden Years Find Peace and Comfort</h1>
-                                        <p class="hero-text" data-ani="slideinup" data-ani-delay="0.6s">Compassionate senior living with dignity, respect, and joy</p>
-                                        <div data-ani="slideinup" data-ani-delay="0.10">
-                                            <a href="{{ route('contact') }}" class="th-btn th-btn-icon2">Get Started <i class="far fa-long-arrow-right ms-3"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="th-hero-bg" data-bg-src="assets/img/Home-img/Personalized-Care-Plans.jpg"></div>
-                    <div class="hero-overlay"></div>
-                    <div class="hero-inner">
-                        <div class="container th-container2">
-                            <div class="row gy-4 gx-40 align-items-center justify-content-center">
-                                <div class="col-xxl-8 col-xl-9 col-lg-10">
-                                    <div class="hero-style7">
-                                        <h1 class="hero-title" data-ani="slideinup" data-ani-delay="0.4s">Personalized Care Tailored to Individual Needs</h1>
-                                        <p class="hero-text" data-ani="slideinup" data-ani-delay="0.6s">Comprehensive support for physical, emotional, and social well-being</p>
-                                        <div data-ani="slideinup" data-ani-delay="0.10">
-                                            <a href="{{ route('contact') }}" class="th-btn th-btn-icon2">Get Started <i class="far fa-long-arrow-right ms-3"></i></a>
-                                        </div>
+    <div class="swiper th-slider" id="heroSlider8" data-slider-options='{"effect":"fade", "autoplay":true, "autoHeight": "true"}'>
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="th-hero-bg" data-bg-src="assets/img/Home-img/personalized-care-tailoring-services-to-individual-needs-hero-1.jpg"></div>
+                <div class="hero-overlay"></div>
+                <div class="hero-inner">
+                    <div class="container th-container2">
+                        <div class="row gy-4 align-items-center justify-content-center text-center">
+                            <div class="col-xxl-8 col-xl-9 col-lg-10">
+                                <div class="hero-style7">
+                                    <h1 class="hero-title" data-ani="slideinup" data-ani-delay="0.4s">A Home Away From Home for Your Loved Ones</h1>
+                                    <p class="hero-text" data-ani="slideinup" data-ani-delay="0.6s">Safe, comfortable assisted living with personalized senior care</p>
+                                    <div data-ani="slideinup" data-ani-delay="0.10">
+                                        <a href="{{ route('contact') }}" class="th-btn th-btn-icon2">Get Started <i class="far fa-long-arrow-right ms-3"></i></a>
                                     </div>
                                 </div>
                             </div>
@@ -74,15 +34,54 @@
                     </div>
                 </div>
             </div>
-
-            <button data-slider-prev="#serviceSlider1" class="slider-arrow style-border2 slider-prev">
-                <img data-mask-src="assets/img/icon/arrow-left.svg" src="assets/img/icon/arrow-left.svg" alt="img">
-            </button>
-            <button data-slider-next="#serviceSlider1" class="slider-arrow style-border2 slider-next">
-                <img data-mask-src="assets/img/icon/arrow-right.svg" src="assets/img/icon/arrow-right.svg" alt="img">
-            </button>
+            <div class="swiper-slide">
+                <div class="th-hero-bg" data-bg-src="assets/img/Home-img/615b679fef36ca74d291148e_Depositphotos_25178721_L.jpg"></div>
+                <div class="hero-overlay"></div>
+                <div class="hero-inner">
+                    <div class="container th-container2">
+                        <div class="row gy-4 align-items-center justify-content-center text-center">
+                            <div class="col-xxl-8 col-xl-9 col-lg-10">
+                                <div class="hero-style7">
+                                    <h1 class="hero-title" data-ani="slideinup" data-ani-delay="0.4s">Where Golden Years Find Peace and Comfort</h1>
+                                    <p class="hero-text" data-ani="slideinup" data-ani-delay="0.6s">Compassionate senior living with dignity, respect, and joy</p>
+                                    <div data-ani="slideinup" data-ani-delay="0.10">
+                                        <a href="{{ route('contact') }}" class="th-btn th-btn-icon2">Get Started <i class="far fa-long-arrow-right ms-3"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="th-hero-bg" data-bg-src="assets/img/Home-img/Personalized-Care-Plans.jpg"></div>
+                <div class="hero-overlay"></div>
+                <div class="hero-inner">
+                    <div class="container th-container2">
+                        <div class="row gy-4 align-items-center justify-content-center text-center">
+                            <div class="col-xxl-8 col-xl-9 col-lg-10">
+                                <div class="hero-style7">
+                                    <h1 class="hero-title" data-ani="slideinup" data-ani-delay="0.4s">Personalized Care Tailored to Individual Needs</h1>
+                                    <p class="hero-text" data-ani="slideinup" data-ani-delay="0.6s">Comprehensive support for physical, emotional, and social well-being</p>
+                                    <div data-ani="slideinup" data-ani-delay="0.10">
+                                        <a href="{{ route('contact') }}" class="th-btn th-btn-icon2">Get Started <i class="far fa-long-arrow-right ms-3"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <button data-slider-prev="#serviceSlider1" class="slider-arrow style-border2 slider-prev">
+            <img data-mask-src="assets/img/icon/arrow-left.svg" src="assets/img/icon/arrow-left.svg" alt="img">
+        </button>
+        <button data-slider-next="#serviceSlider1" class="slider-arrow style-border2 slider-next">
+            <img data-mask-src="assets/img/icon/arrow-right.svg" src="assets/img/icon/arrow-right.svg" alt="img">
+        </button>
     </div>
+</div>
 
 <style>
 .hero-overlay {
@@ -91,16 +90,44 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.5); /* Dark overlay with 50% opacity */
+    background: rgba(0, 0, 0, 0.5);
     z-index: 1;
 }
 
 .hero-inner {
     position: relative;
-    z-index: 2; /* Ensures text appears above overlay */
+    z-index: 2;
 }
 
 .swiper-slide {
     position: relative;
 }
+
+/* Reduced font size for hero titles */
+.hero-title {
+    font-size: 2.5rem !important; /* Adjust this value as needed */
+    line-height: 1.2 !important;
+}
+
+/* Center alignment enhancements */
+.text-center {
+    text-align: center !important;
+}
+
+
+/* Responsive font sizes */
+@media (max-width: 768px) {
+    .hero-title {
+        font-size: 1.8rem !important;
+    }
+}
+
+@media (max-width: 576px) {
+    .hero-title {
+        font-size: 1.5rem !important;
+    }
+}
+
+
+
 </style>
