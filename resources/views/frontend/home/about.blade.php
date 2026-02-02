a5c13d61207e45f2b4a4300fcde4eb7fcd645122
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-02 10:48:20 +0530

    Update about us section

--- resources/views/frontend/home/about.blade.php
+++ resources/views/frontend/home/about.blade.php
@@ -16,13 +16,13 @@
                         <div class="col-lg-6">
                             <div class="row g-4">
                                 <div class="col-6">
-                                    <img src="assets\img\Home-img\hero55-1024x938.webp" class="img-fluid rounded-10 mb-4 w-70 ms-30 wow scaleIn" alt="">
-                                    <img src="images/misc/3.webp" class="img-fluid rounded-10 wow scaleIn" alt="">
+                                    <img src="assets\img\Home-img\galary-img2.png" class="img-fluid rounded-10 mb-4 w-70 ms-30 wow scaleIn" alt="">
+                                    <img src="assets\img\Home-img\galaryimg1.png" class="img-fluid rounded-10 wow scaleIn" alt="">
                                 </div>
                                 <div class="col-6">
                                     <div class="spacer-single sm-hide"></div>
-                                    <img src="images/misc/10.webp" class="img-fluid rounded-10 mb-4 wow scaleIn" alt="">
-                                    <img src="images/misc/8.webp" class="img-fluid rounded-10 w-70 wow scaleIn" alt="">
+                                    <img src="assets\img\Home-img\02f06849_560x560.jpg" class="img-fluid rounded-10 mb-4 wow scaleIn" alt="">
+                                    <img src="assets\img\Home-img\private-duty-caregiver-2.jpeg" class="img-fluid rounded-10 w-70 wow scaleIn" alt="">
                                 </div>
                             </div>
                         </div>
@@ -31,38 +31,38 @@
                 </div>
             </section>
 
-<section class="pt-0">
-    <div class="container">
-        <div class="row g-4">
-            <div class="col-lg-4 col-md-6">
-                <div class="relative">
-                    <img src="images/icons/black/medical-care-icon.webp" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Medical care icon">
-                    <div class="relative ps-100 ms-4 wow fadeInUp">
-                        <h4>Comprehensive Medical Care</h4>
-                        <p class="mb-0">Tailored medical support with dedicated professionals committed to compassionate care and overall well-being.</p>
-                    </div>
-                </div>
-            </div>
+            <section class="pt-0">
+                <div class="container">
+                    <div class="row g-4">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="relative">
+                                <img src="images/icons/black/medical-care-icon.webp" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Medical care icon">
+                                <div class="relative ps-100 ms-4 wow fadeInUp">
+                                    <h4>Comprehensive Medical Care</h4>
+                                    <p class="mb-0">Tailored medical support with dedicated professionals committed to compassionate care and overall well-being.</p>
+                                </div>
+                            </div>
+                        </div>
 
-            <div class="col-lg-4 col-md-6">
-                <div class="relative">
-                    <img src="images/icons/black/nutrition-icon.webp" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Nutrition icon">
-                    <div class="relative ps-100 ms-4 wow fadeInUp">
-                        <h4>Nutritious Meals</h4>
-                        <p class="mb-0">Meticulously crafted meals celebrating wholesome ingredients and culinary excellence for optimal health.</p>
-                    </div>
-                </div>
-            </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="relative">
+                                <img src="images/icons/black/nutrition-icon.webp" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Nutrition icon">
+                                <div class="relative ps-100 ms-4 wow fadeInUp">
+                                    <h4>Nutritious Meals</h4>
+                                    <p class="mb-0">Meticulously crafted meals celebrating wholesome ingredients and culinary excellence for optimal health.</p>
+                                </div>
+                            </div>
+                        </div>
 
-            <div class="col-lg-4 col-md-6">
-                <div class="relative">
-                    <img src="images/icons/black/activities-icon.webp" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Activities icon">
-                    <div class="relative ps-100 ms-4 wow fadeInUp">
-                        <h4>Recreational Activities</h4>
-                        <p class="mb-0">Engaging social programs designed to ignite passions, foster connections, and enrich life with creativity.</p>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="relative">
+                                <img src="images/icons/black/activities-icon.webp" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Activities icon">
+                                <div class="relative ps-100 ms-4 wow fadeInUp">
+                                    <h4>Recreational Activities</h4>
+                                    <p class="mb-0">Engaging social programs designed to ignite passions, foster connections, and enrich life with creativity.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</section>
+            </section>
