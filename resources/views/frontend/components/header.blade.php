333fd5ffee004c7301d09526a424bd2701c5902e
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-02 14:06:14 +0530

    Add gallery Page

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -23,8 +23,8 @@
                                     <li><a class="menu-item" href="{{ route('services') }}">Services</a></li>
                                     <li><a class="menu-item" href="{{ route('howitworks') }}">How It Works</a></li>
                                     <li><a class="menu-item" href="{{ route('about') }}">About</a></li>
-                                    <li><a class="menu-item" href="projects.html">Projects</a></li>
-                                    <li><a class="menu-item" href="blog.html">Blog</a></li>
+                                    <li><a class="menu-item" href="{{ route('gallery') }}">Gallery</a></li>
+                                    <li><a class="menu-item" href="{{ route('blog') }}">Blog</a></li>
                                     <li><a class="menu-item" href="{{ route('contact') }}">Contact</a></li>
                                 </ul>
                             </div>
