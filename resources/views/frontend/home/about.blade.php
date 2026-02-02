e33f7d7dc05420fde7a69eedc56db295056fbd66
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-02 16:55:19 +0530

    Update home page

--- resources/views/frontend/home/about.blade.php
+++ resources/views/frontend/home/about.blade.php
@@ -38,8 +38,8 @@
                             <div class="relative">
                                 <img src="assets/images/icons/black/673476347.png" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Medical care icon">
                                 <div class="relative ps-100 ms-4 wow fadeInUp">
-                                    <h4>Comprehensive Medical Care</h4>
-                                    <p class="mb-0">Tailored medical support with dedicated professionals committed to compassionate care and overall well-being.</p>
+                                    <h4>Holistic Approach</h4>
+                                    <p class="mb-0">Emphasize your holistic approach to care, addressing not only physical needs but also mental, emotional, and spiritual well-being.</p>
                                 </div>
                             </div>
                         </div>
@@ -48,8 +48,8 @@
                             <div class="relative">
                                 <img src="assets/images/icons/black/1422994-200.png" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Nutrition icon">
                                 <div class="relative ps-100 ms-4 wow fadeInUp">
-                                    <h4>Nutritious Meals</h4>
-                                    <p class="mb-0">Meticulously crafted meals celebrating wholesome ingredients and culinary excellence for optimal health.</p>
+                                    <h4>Customized Care Plans</h4>
+                                    <p class="mb-0">Highlight your personalized care plans tailored to each resident's unique preferences, needs, and goals</p>
                                 </div>
                             </div>
                         </div>
@@ -58,8 +58,39 @@
                             <div class="relative">
                                 <img src="assets/images/icons/black/57436776.png" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Activities icon">
                                 <div class="relative ps-100 ms-4 wow fadeInUp">
-                                    <h4>Recreational Activities</h4>
-                                    <p class="mb-0">Engaging social programs designed to ignite passions, foster connections, and enrich life with creativity.</p>
+                                    <h4>Comprehensive Services</h4>
+                                    <p class="mb-0">Showcase the breadth of services you offer, including not just basic care needs but also additional services such as physiotherapy, spiritual support, and transportation assistance.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="row g-4 mt-4">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="relative">
+                                <img src="assets/images/icons/black/564564.png" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Medical care icon">
+                                <div class="relative ps-100 ms-4 wow fadeInUp">
+                                    <h4>24/7 Availability</h4>
+                                    <p class="mb-0">Highlight your round-the-clock availability of staff, ensuring residents have support and assistance whenever they need it.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <div class="relative">
+                                <img src="assets/images/icons/black/6546564.png" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Nutrition icon">
+                                <div class="relative ps-100 ms-4 wow fadeInUp">
+                                    <h4>Quality & friendly Staff</h4>
+                                    <p class="mb-0">Emphasize the qualifications, training, and compassionate nature of your staff, reassuring families that their loved ones are in capable hands.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <div class="relative">
+                                <img src="assets/images/icons/black/654654.png" class="abs bg-color-2 w-100px p-15 rounded-10 mb-3 wow scaleIn" alt="Activities icon">
+                                <div class="relative ps-100 ms-4 wow fadeInUp">
+                                    <h4>Community and Social Engagement</h4>
+                                    <p class="mb-0">Showcase the vibrant community and engaging social activities available to residents, promoting a sense of belonging and purpose.</p>
                                 </div>
                             </div>
                         </div>
