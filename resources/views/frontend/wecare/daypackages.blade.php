46676361edcdd6e5a21e6813746819348b05db6f
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-03 11:26:10 +0530

    Update Day package section

--- resources/views/frontend/wecare/daypackages.blade.php
+++ resources/views/frontend/wecare/daypackages.blade.php
@@ -1,69 +1,83 @@
-    <section class="feature-area-1 position-relative space overflow-hidden bg-white" id="service-sec">
 
-        <div class="container th-container2">
-            <div class="row justify-content-center">
-                <div class="col-xl-8 col-lg-10">
-                    <div class="title-area text-center">
-                        <span class="sub-title style2 text-anim" data-cue="slideInUp">Day Packages</span>
-                    </div>
-                </div>
-            </div>
-            <div class="row gy-4 justify-content-center">
-                <div class="col-xl-3 col-lg-6 col-md-6 bg-white">
-                    <div class="feature-card bg-white" data-cue="slideInUp">
-                        <div class="top-wrap" 
-                            style="display:flex; flex-direction:column; align-items:center; text-align:center;">                            <div class="box-icon" style="margin-bottom:1rem;">
-                                <img src="assets/img/icon/11328270.png" alt="icon">
-                            </div>
-                            <h3 class="box-title">Companion Care Package</h3>
-                            <p class="box-text">5000 LKR</p>
-                        </div>
-                        <div class="bottom-text">
-
-                            <a class="th-btn th-btn-sm w-100" href="{{ route('contact') }}">Enquire Now</a>
+            <section class="bg-color text-light">
+                <div class="container">
+                    <div class="row g-4">
+                        <div class="col-lg-6 offset-lg-3 text-center">
+                            <div class="subtitle wow fadeInUp mb-3">Day Packages</div>
+                            <h2 class="wow fadeInUp" data-wow-delay=".2s">Elder Day Care Plans</h2>
+                            <p class="lead mb-0 wow fadeInUp">
+                                Comfortable, safe, and caring day services designed to support seniors and give families peace of mind.
+                            </p>
+                            <div class="spacer-single"></div>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-lg-6 col-md-6">
-                    <div class="feature-card bg-white" data-cue="slideInUp">
-                        <div class="top-wrap" 
-                            style="display:flex; flex-direction:column; align-items:center; text-align:center;">                            <div class="box-icon" style="margin-bottom:1rem;">
-                                <img src="assets/img/icon/5580956.png" alt="icon">
+            </section>
+
+
+            <section class="pt-0 pb-0">
+                <div class="container">
+                    <div class="row g-4 mt-min-100">
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="relative bg-light p-4 rounded-10">
+                                <div class="text-center">
+                                    <h5 class="mb-0">Companion Care Package</h5>
+                                    <div class="spacer-30"></div>
+                                    <div class="ms-5 fw-500">
+                                        
+                                        <span class="fs-64 fw-bold text-dark">5000</span>
+                                        <span>LKR</span>
+                                         <span>/Day</span>
+                                    </div>
+                                    <div class="spacer-20"></div>
+                                </div>
+
+                                <div class="text-center">
+                                    <a class="btn-main bg-color-2 text-dark" href="{{ route('contact') }}">Book Now</a>
+                                </div>
+                                <div class="spacer-20"></div>
                             </div>
-                            <h3 class="box-title">Shared Comfort Package</h3>
-                            <p class="box-text">6500 LKR</p>
                         </div>
-                        <div class="bottom-text">
-                            <div class="checklist style2">
-   
+
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="relative bg-light p-4 rounded-10">
+                                <div class="text-center">
+                                    <h5 class="mb-0">Shared Comfort Package</h5>
+                                    <div class="spacer-30"></div>
+                                    <div class="ms-5 fw-500">
+                                        <span class="fs-64 fw-bold text-dark">6500</span>
+                                        <span>LKR</span>
+                                        <span>/Day</span>
+                                    </div>
+                                    <div class="spacer-20"></div>
+                                </div>
+
+                                <div class="text-center">
+                                    <a class="btn-main bg-color-2 text-dark" href="{{ route('contact') }}">Book Now</a>
+                                </div>
+                                <div class="spacer-20"></div>
                             </div>
-                            <a class="th-btn th-btn-sm w-100" href="{{ route('contact') }}">Enquire Now </a>
+                        </div>
+
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="relative bg-light p-4 rounded-10">
+                                <div class="text-center">
+                                    <h5 class="mb-0">Private Heaven Package</h5>
+                                    <div class="spacer-30"></div>
+                                    <div class="ms-5 fw-500">
+                                        <span class="fs-64 fw-bold text-dark">9500</span>
+                                        <span>LKR</span>
+                                        <span>/Day</span>
+                                    </div>
+                                    <div class="spacer-20"></div>
+                                </div>
+
+                                <div class="text-center">
+                                    <a class="btn-main bg-color-2 text-dark" href="{{ route('contact') }}">Book Now</a>
+                                </div>
+                                <div class="spacer-20"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-lg-6 col-md-6">
-                    <div class="feature-card bg-white" data-cue="slideInUp">
-                        <div class="top-wrap" 
-                            style="display:flex; flex-direction:column; align-items:center; text-align:center;">
-                            
-                            <div class="box-icon" style="margin-bottom:1rem;">
-                                <img src="assets/img/icon/9324070.png" alt="icon">
-                            </div>
-
-                            <h3 class="box-title">Private Heaven Package</h3>
-                            <p class="box-text">9500 LKR</p>
-                        </div>
-
-                        <div class="bottom-text">
-                            <a class="th-btn th-btn-sm w-100" href="{{ route('contact') }}">
-                                Enquire Now
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-   
+            </section>    
