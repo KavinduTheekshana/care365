2c43889fb2adce4ade28c649539dbd263f4ba659
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-05 12:01:09 +0530

    Hide team section

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -20,7 +20,6 @@
     @include('frontend.about.hero')
     @include('frontend.about.journey')
     @include('frontend.about.about')
-    @include('frontend.about.team')
     @include('frontend.about.careers')
 
 @endsection
