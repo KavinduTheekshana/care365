b46eae197dd5b494f5b4a01b2ffade8c7c9374de
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-08 20:07:32 +0530

    Update about us controller

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -14,27 +14,11 @@ class AboutUsController extends Controller
 {
     public function index(): View
     {
-        // Get random 8 services for the slider
-        $services = Service::where('is_public', true)
-                          ->inRandomOrder()
-                          ->take(8)
-                          ->get();
-        
-        // Get all public Who We Are items ordered by display_order
-        $whoweares = Whoweare::where('is_public', true)
-                            ->orderBy('display_order')
-                            ->get();
-        
-
-        $testimonials = Testimonial::where('is_public', true)
-                            ->inRandomOrder()
-                             ->take(4)
-                            ->get();
 
         $teamMembers = Team::where('active', true)
                            ->orderBy('id')  
                            ->get();
         
-        return view('frontend.about.index', compact('services', 'whoweares',  'testimonials', 'teamMembers'));
+        return view('frontend.about.index', compact('teamMembers'));
     }
 }
