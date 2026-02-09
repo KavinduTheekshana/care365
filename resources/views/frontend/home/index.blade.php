0ecaf794a96b004d7f760c5069bd90dbacea2634
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-02-09 12:40:34 +0530

    Update home page

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -24,6 +24,7 @@
 @section('content')
     @include('frontend.home.hero_2')
     @include('frontend.home.about')
+    @include('frontend.home.leadmagnet')
     @include('frontend.home.services')
     @include('frontend.home.carehome')
     @include('frontend.home.packages')
